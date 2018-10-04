93ea287318fb832f4d2f58c27ec501ae2d05ab3e
Simula Kristoffer <simulak1@sagan.org.aalto.fi>
2018-10-04 08:19:23 +0300

    Deleted unnecessary files

--- tools/wrapper.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup,Extension
-
-module1=Extension('doublef',sources=['ewaldmodule.c'])
-
-setup(name='doublef',version='1.0',description='This is a test package',ext_modules=[module1])
