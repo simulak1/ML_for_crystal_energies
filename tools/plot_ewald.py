1d24d9ebd2e906d34d7cdf3cedea6ae4b38845a8
Simula Kristoffer <simulak1@sagan.org.aalto.fi>
2018-10-05 15:56:35 +0300

    Final plotting tests can be seen here.

--- tools/plot_ewald.py
+++ tools/plot_ewald.py
@@ -1,13 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-m1=np.load('M1.npy')
-m2=np.load('M2.npy')
-print(m1.shape)
-plt.figure(1)
-plt.imshow(m1)
-plt.colorbar()
-plt.figure(2)
-plt.imshow(m2)
+m=np.load('results.npy')
+m2=np.load('results2.npy')
+
+M=m[0,2,:,:]-m2[0,2,:,:]
+plt.imshow(M)
 plt.colorbar()
 plt.show()
+#ind1=np.random.randint(80,size=10)
+#ind2=np.random.randint(80,size=10)
+
+#xx=10*np.arange(1,10)
+
+#fig, ((ax1,ax2),(ax3,ax4))=plt.subplots(2,2)
+
+
+#for i in range(10):
+#    ax1.plot(xx,m[:,0,ind1[i],ind2[i]])
+#for i in range(10):
+#    ax2.plot(xx,m[:,1,ind1[i],ind2[i]])
+#for i in range(10):
+#    ax3.plot(xx,m[:,2,ind1[i],ind2[i]])
+#for i in range(10):
+#    ax4.plot(xx,m[:,3,ind1[i],ind2[i]])
+#plt.show()
+
+#plt.figure(1)
+#plt.imshow(m[0,0,:,:])
+#plt.colorbar()
+#plt.show()
