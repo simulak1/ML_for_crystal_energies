a2f48e1401558acbf033d7212c7ac1fbf0fa96e6
Simula Kristoffer <simulak1@sagan.org.aalto.fi>
2018-10-05 15:54:13 +0300

    wrapper compiler contains now options for openmp

--- c_functions/wrapper.py
+++ c_functions/wrapper.py
@@ -1,6 +1,6 @@
 from distutils.core import setup,Extension
 import numpy
 
-module1=Extension('_C_arraytest',sources=['C_arraytest.c'])
+module1=Extension('_C_arraytest',sources=['C_arraytest.c'],extra_compile_args=['-fopenmp'],extra_link_args=['-lgomp'])
 
 setup(name='double',version='1.0',description='This is a test package',ext_modules=[module1])
