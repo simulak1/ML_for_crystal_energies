35b24affff3bb71d41f54b45e631a286d6aca931
Simula Kristoffer <simulak1@sagan.org.aalto.fi>
2018-11-05 11:20:41 +0200

    Simplified the data-loading routines, now all 3000 structures and labels can be handled

--- src/data.py
+++ src/data.py
@@ -9,7 +9,7 @@ class Material:
         self.La=La
         self.xyz=xyz
         self.Z=Z
-        self.spacegrp=spacegrp
+        self.spacegrp=int(spacegrp)
         self.p_al=p_al
         self.p_ga=p_ga
         self.p_in=p_in
@@ -22,6 +22,35 @@ class Material:
         self.Ef=Ef
         self.Eb=Eb
 
+def parseGeometryFile(file):
+    La=np.zeros((3,3))
+    with open(file) as f:
+        lines=f.readlines()
+        Nlines=len(lines)
+        Natoms=Nlines-6
+        # Read lattice vectors
+        for j in range(3,6):
+            line=lines[j].split()
+            for k in range(1,4):
+                La[j-3,k-1]=float(line[k])
+        # Atom coordinates and atomic numbers
+        Z=np.zeros((Natoms,))
+        xyz=np.zeros((Natoms,3))
+        for j in range(6,Nlines):
+            line=lines[j].split()
+            xyz[j-6,0]=float(line[1])
+            xyz[j-6,1]=float(line[2])
+            xyz[j-6,2]=float(line[3])
+            if(line[4]=="Ga"):
+                Z[j-6]=31
+            elif(line[4]=="Al"):
+                Z[j-6]=13
+            elif(line[4]=="O"):
+                Z[j-6]=8
+            elif(line[4]=="In"):
+                Z[j-6]=49
+        return La,xyz,Z
+                
 def load_data(indices,datapath):
     ''' 
     Takes in the number of desired datapoints, picks random indices 
@@ -29,34 +58,12 @@ def load_data(indices,datapath):
     '''
 
     materials=[]
-    rawdata=pandas.read_csv(datapath+"/data/train.csv",header=None)
+    data=np.load(datapath+"/data/data.npy")
     for i in indices:
-        La=np.zeros((3,3))
-        with open(datapath+"/data/train/"+str(i+1)+"/geometry.xyz") as f:
-            lines=f.readlines()
-            Nlines=len(lines)
-            Natoms=Nlines-6
-            # Read lattice vectors
-            for j in range(3,6):
-                line=lines[j].split()
-                for k in range(1,4):
-                    La[j-3,k-1]=float(line[k])
-            # Atom coordinates and atomic numbers
-            Z=np.zeros((Natoms,))
-            xyz=np.zeros((Natoms,3))
-            for j in range(6,Nlines):
-                line=lines[j].split()
-                xyz[j-6,0]=float(line[1])
-                xyz[j-6,1]=float(line[2])
-                xyz[j-6,2]=float(line[3])
-                if(line[4]=="Ga"):
-                    Z[j-6]=31
-                elif(line[4]=="Al"):
-                    Z[j-6]=13
-                elif(line[4]=="O"):
-                    Z[j-6]=8
-                elif(line[4]=="In"):
-                    Z[j-6]=49
-        materials.append(Material(La,xyz,Z,int(rawdata[1][i+1]),float(rawdata[2][i+1]),float(rawdata[3][i+1]),float(rawdata[4][i+1]),float(rawdata[5][i+1]),float(rawdata[6][i+1]),float(rawdata[7][i+1]),float(rawdata[8][i+1]),float(rawdata[9][i+1]),float(rawdata[10][i+1]),float(rawdata[11][i+1]),float(rawdata[12][i+1]),float(rawdata[13][i+1])))
-    return(materials)
-
+        if(i<2400):
+            file=datapath+"/data/train/"+str(i+1)+"/geometry.xyz"
+        else:
+            file=datapath+"/data/test/"+str(i+1-2400)+"/geometry.xyz"
+        La,xyz,Z=parseGeometryFile(file)
+        materials.append(Material(La,xyz,Z,data[i,1],data[i,2],data[i,3],data[i,4],data[i,5],data[i,6],data[i,7],data[i,8],data[i,9],data[i,10],data[i,11],data[i,12],data[i,13]))
+    return materials
